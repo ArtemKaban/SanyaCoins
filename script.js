ae2de8e8f164aeb0678e9ba07b4e37980e781032
ArtemKaban <artmekabansky001@gmail.com>
2024-08-23 22:45:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var san = document.getElementById('san');
 var p = document.getElementById('s');
 var counter = 0;
-
+var p2 = document.getElementById('p2');
 
 
 san.addEventListener("click",function()
@@ -9,7 +9,25 @@ san.addEventListener("click",function()
     counter++;
     p.textContent = counter;
     console.log(counter);
-    if(counter == 50){
+    if(counter == 100){
         san.src = "img/krutoisan.png";
+        p2.textContent = 'Крутой Саня';
+    }
+    if(counter == 250){
+        san.src = "img/adultsan.png";
+        p2.textContent = 'подросток саня';
+    }
+    if(counter == 500){
+        san.src = "img/Shava.png";
+        p2.textContent = 'саня с шавухой';
+    }
+    if(counter == 750){
+        san.src = "img/pila.png";
+        p2.textContent = 'саня гробовщик';
+    }
+    if(counter == 1000){
+        san.src = "img/viduha.png";
+        p2.textContent = 'саня с видюхой';
     }
 });
+

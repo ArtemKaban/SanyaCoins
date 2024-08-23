b82ade46e56d76ca2349be21be4e5712038a3827
ArtemKaban <artmekabansky001@gmail.com>
2024-08-23 21:28:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var san = document.getElementById('san');
 var p = document.getElementById('s');
 var counter = 0;
+
+
+
 san.addEventListener("click",function()
 {
     counter++;
     p.textContent = counter;
+    console.log(counter);
+    if(counter == 50){
+        san.src = "img/krutoisan.png";
+    }
 });
